2ad6eec2bb0fead91e3b219a4883eaf4b0c72330
kevin-wairi <kevinwairi@gmail.com>
2024-01-24 15:55:48 +0300

    update cor.rb 7

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,8 +13,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
               /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
               # regular expressions can be used here
   
-      resource '/file/list_all/', :headers => 'x-domain-token'
-      resource '/file/at/*',
+      resource '*', :headers => 'x-domain-token'
+      resource '*',
           methods: [:get, :post, :delete, :put, :patch, :options, :head],
           headers: 'x-domain-token',
           expose: ['Some-Custom-Response-Header'],
@@ -24,7 +24,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   
     allow do
         origins "https://frontend-knowedgequest.onrender.com"
-      resource '/public/*', headers: :any, methods: :get
+      resource '*', headers: :any, methods: :get
   
       # Only allow a request for a specific host
       resource '/api/v1/*',

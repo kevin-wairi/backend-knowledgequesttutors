1c9c9a817046b3ed7280628a6c23d2a0a6550e9f
kevin-wairi <kevinwairi@gmail.com>
2024-01-24 16:58:07 +0300

    update cor.rb 8

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,30 +6,31 @@
 # Read more: https://github.com/cyu/rack-cors
 # config/initializers/cors.rb
 
-  
+# config/initializers/cors.rb
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins "https://frontend-knowedgequest.onrender.com"
-              /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
-              # regular expressions can be used here
+      origins "https://frontend-knowledgequest.onrender.com", /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
   
-      resource '*', :headers => 'x-domain-token'
       resource '*',
-          methods: [:get, :post, :delete, :put, :patch, :options, :head],
-          headers: 'x-domain-token',
-          expose: ['Some-Custom-Response-Header'],
-          max_age: 720
-          # headers to expose
+        headers: :any,
+        methods: [:get, :post, :delete, :put, :patch, :options, :head],
+        expose: ['Some-Custom-Response-Header'],
+        max_age: 720
     end
   
     allow do
-        origins "https://frontend-knowedgequest.onrender.com"
-      resource '*', headers: :any, methods: :get
+      origins "https://frontend-knowledgequest.onrender.com"
+      
+      resource '*',
+        headers: :any,
+        methods: :get
   
       # Only allow a request for a specific host
-      resource '/api/v1/*',
-          headers: :any,
-          methods: :get,
-          if: proc { |env| env['HTTP_HOST'] == 'https://frontend-knowedgequest.onrender.com' }
+      resource '*',
+        headers: :any,
+        methods: :get,
+        if: proc { |env| env['HTTP_HOST'] == 'https://frontend-knowledgequest.onrender.com' }
     end
   end
+  

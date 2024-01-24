96be7d558a7ede409504d2b6da41a9c148c1578a
kevin-wairi <kevinwairi@gmail.com>
2024-01-24 17:59:08 +0300

    update user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id,:username,:school, :email,:phoneNumber,:course,:img,:isAdmin
-  has_many :chats, through: :messages
-  has_many :questions
-  has_many :messages
+  # has_many :chats, through: :messages
+  # has_many :questions
+  # has_many :messages
 end

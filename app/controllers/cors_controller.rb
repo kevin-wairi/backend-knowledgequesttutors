a97269d35775574d7bbc9c74c284d329d967f358
kevin-wairi <kevinwairi@gmail.com>
2024-01-24 11:03:34 +0300

    update cors controller

--- app/controllers/cors_controller.rb
+++ app/controllers/cors_controller.rb
@@ -10,7 +10,8 @@ class CorsController < ApplicationController
     protected
 
     def cors_set_access_control_headers
-        response.headers['Access-Control-Allow-Origin'] = "https://frontend-knowedgequest.onrender.com/"
+        response.headers['Access-Control-Allow-Origin'] = "https://frontend-knowedgequest.onrender.com"
+
         response.headers['Access-Control-Allow-Credentials'] = "true"
         response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
         response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'

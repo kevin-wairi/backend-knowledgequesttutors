2a2181207d0c4e33d0bfd0053bdeb5353c86e162
kevin-wairi <kevinwairi@gmail.com>
2024-01-24 14:55:44 +0300

    update cor.rb 2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,24 +16,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         credentials: true  # Allow credentials (cookies) in the request
     end
   
-    # Add a separate block to handle errors for disallowed origins
-    allow do
-        origins "https://frontend-knowedgequest.onrender.com"  # Add your allowed origins here
-  
-      resource "*",
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true  # Allow credentials (cookies) in the request
-  
-      # Handling CORS error for disallowed origin
-      # You can customize the response based on your requirements
-      if ENV['RAILS_ENV'] == 'production'
-        # In production, you might want to return a more generic error message
-        run lambda { |_env| [403, { 'Content-Type' => 'application/json' }, [{ error: 'CORS not allowed' }.to_json]] }
-      else
-        # In development, you can provide more detailed error information
-        run lambda { |_env| [403, { 'Content-Type' => 'application/json' }, [{ error: 'CORS not allowed from this origin' }.to_json]] }
-      end
-    end
+   
   end
   

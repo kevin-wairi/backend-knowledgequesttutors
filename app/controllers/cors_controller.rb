3ae77b678d4d8de63b84dcbdf87dd469147eab0e
kevin-wairi <kevinwairi@gmail.com>
2024-01-24 13:04:48 +0300

    update cors controller

--- app/controllers/cors_controller.rb
+++ app/controllers/cors_controller.rb
@@ -1,23 +1,23 @@
 class CorsController < ApplicationController
     skip_before_action :authorized
-    
+
     def cors_preflight_check
         if request.method == 'OPTIONS'
-            cors_set_access_control_headers
-            render  text: '', content_type: 'text/plain'
+            self.class.cors_set_access_control_headers(response.headers)
+            head :ok
             puts "OKKKKKKKKKKKKKRRRRRRRRR"
         end
     end
 
     protected
 
-    def cors_set_access_control_headers
+    def self.cors_set_access_control_headers(headers)
         response.headers['Access-Control-Allow-Origin'] = "https://frontend-knowedgequest.onrender.com"
 
         response.headers['Access-Control-Allow-Credentials'] = "true"
         response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
         response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
-        response.headers['Access-Control-Max-Age'] = "86400"
+        response.headers['Access-Control-Max-Age'] = "7200"
     end
 
 end

215d61959c56acd5462376f80a508b11e26aab3a
kevin-wairi <kevinwairi@gmail.com>
2024-01-24 15:39:47 +0300

    update cor.rb 4

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,16 +6,30 @@
 # Read more: https://github.com/cyu/rack-cors
 # config/initializers/cors.rb
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins "https://frontend-knowedgequest.onrender.com"
   
-      resource "*",
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true  # Allow credentials (cookies) in the request
+use Rack::Cors do
+    allow do
+        origins "https://frontend-knowedgequest.onrender.com"
+              /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
+              # regular expressions can be used here
+  
+      resource '/file/list_all/', :headers => 'x-domain-token'
+      resource '/file/at/*',
+          methods: [:get, :post, :delete, :put, :patch, :options, :head],
+          headers: 'x-domain-token',
+          expose: ['Some-Custom-Response-Header'],
+          max_age: 720
+          # headers to expose
     end
   
-   
-end
+    allow do
+        origins "https://frontend-knowedgequest.onrender.com"
+      resource '/public/*', headers: :any, methods: :get
   
+      # Only allow a request for a specific host
+      resource '/api/v1/*',
+          headers: :any,
+          methods: :get,
+          if: proc { |env| env['HTTP_HOST'] == 'https://frontend-knowedgequest.onrender.com' }
+    end
+  end
